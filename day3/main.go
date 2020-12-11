38d5fb2a42fd7792ee3e6d0700c49add9ef8a57d
moosebot <austinvalle@gmail.com>
2020-12-11 01:37:29 -0500

    solved day 3, part 2

--- day3/main.go
+++ day3/main.go
@@ -3,6 +3,7 @@ package day3
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 )
@@ -16,26 +17,46 @@ type password struct {
 
 // RunSolution - https://adventofcode.com/2020/day/3
 func RunSolution() error {
-	// How many trees would you encounter, starting at the top left, and going right 3, down 1
 	mapFile, err := os.Open("./day3/map.txt")
 	if err != nil {
 		return err
 	}
-	fileScanner := bufio.NewScanner(mapFile)
 
+	// Part 1
+	numOfTrees := calculateTreesInRoute(mapFile, 3, 1)
+	fmt.Printf("Day 3, Part 1 Result: %d trees encountered\n", numOfTrees)
+
+	// Part 2
+	trees1 := calculateTreesInRoute(mapFile, 1, 1)
+	trees2 := calculateTreesInRoute(mapFile, 3, 1)
+	trees3 := calculateTreesInRoute(mapFile, 5, 1)
+	trees4 := calculateTreesInRoute(mapFile, 7, 1)
+	trees5 := calculateTreesInRoute(mapFile, 1, 2)
+	part2Result := trees1 * trees2 * trees3 * trees4 * trees5
+	fmt.Printf("Day 3, Part 2 Result: %d\n", part2Result)
+
+	return nil
+}
+
+func calculateTreesInRoute(mapFile *os.File, spacesRight int, spacesDown int) int {
+	// Set file pointer to beginning to allow reuse of file
+	mapFile.Seek(0, io.SeekStart)
+
+	fileScanner := bufio.NewScanner(mapFile)
 	// Skip the first line, since the toboggon starts by moving down
 	fileScanner.Scan()
 
 	// Starts at top left (using zero since we'll use as index)
 	xCor := 0
-
 	numOfTrees := 0
-	for fileScanner.Scan() {
+
+	// Will attempt to skip down the amount of spaces, if no more lines will return false
+	for scanLines(fileScanner, spacesDown) {
 		mapLine := fileScanner.Text()
 		if len(mapLine) < 1 {
 			break
 		}
-		xCor += 3
+		xCor += spacesRight
 
 		// This will account for the arboreal genetics and biome stability (┛ಠ_ಠ)┛彡┻━┻
 		if dups := xCor / len(mapLine); dups > 0 {
@@ -49,8 +70,16 @@ func RunSolution() error {
 		}
 	}
 
-	fmt.Printf("Day 3, Part 1 Result: %d trees encountered", numOfTrees)
-	return nil
+	return numOfTrees
+}
+
+func scanLines(scanner *bufio.Scanner, num int) bool {
+	for i := 0; i < num; i++ {
+		if !scanner.Scan() {
+			return false
+		}
+	}
+	return true
 }
 
 func expandMapLine(mapPattern string, dups int) string {
