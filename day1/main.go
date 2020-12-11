f92d6485c2079d791609928f507ac2ed8142ad00
moosebot <austinvalle@gmail.com>
2020-12-10 20:29:28 -0500

    solved for 2 line items that match 2020

--- day1/main.go
+++ day1/main.go
@@ -1,6 +1,7 @@
 package day1
 
 import (
+	"fmt"
 	"io/ioutil"
 	"strconv"
 	"strings"
@@ -15,10 +16,18 @@ func RunSolution() error {
 		return err
 	}
 
-	_, err = getExpenseLineItems(string(expenseReportBytes))
+	expenseItems, err := getExpenseLineItems(string(expenseReportBytes))
 	if err != nil {
 		return err
 	}
+
+	result, err := find2020Expense(expenseItems)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Day 1 Result: %d", result)
 
 	return nil
 }
@@ -39,4 +48,16 @@ func getExpenseLineItems(expenseReport string) (expenseLineItems []int, err erro
 	}
 
 	return expenseLineItems, nil
+}
+
+func find2020Expense(expenseItems []int) (int, error) {
+	for idx1, expense1 := range expenseItems {
+		for idx2, expense2 := range expenseItems {
+			if idx1 != idx2 && expense1+expense2 == 2020 {
+				return expense1 * expense2, nil
+			}
+		}
+	}
+
+	return 0, fmt.Errorf("Couldn't find two numbers in the expense report adding to 2020")
 }
